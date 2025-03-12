59cfc305673a30a1b281f3b0f52a4c2d6f7a65e7
KazMns <119262960+KazMns@users.noreply.github.com>
2025-03-11 21:51:24 -0700

    Add files via upload

---
+++ themeswitcher.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const themeToggle = document.getElementById("theme-toggle");
+  const currentTheme = localStorage.getItem("theme") || "dark";
+
+  document.documentElement.setAttribute("data-theme", currentTheme);
+  themeToggle.textContent = currentTheme === "dark" ? "🌙" : "☀️";
+
+  themeToggle.addEventListener("click", function () {
+    let newTheme =
+      document.documentElement.getAttribute("data-theme") === "dark"
+        ? "light"
+        : "dark";
+    document.documentElement.setAttribute("data-theme", newTheme);
+    localStorage.setItem("theme", newTheme);
+    themeToggle.textContent = newTheme === "dark" ? "🌙" : "☀️";
+  });
+});
